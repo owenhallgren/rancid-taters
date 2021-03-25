eb027bcb08df84830419e75062d27925ab073380
Owen Hallgren <owenhallgren@gmail.com>
2021-03-24 20:46:47 -0600

    Adding styling to poster

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,13 @@
+import './CardStyles.css'
+
 const Card = ({ title, posterPath, averageRating, releaseDate }) => {
 return (
-    <>
-      <p>{title}</p>
-      <p>{posterPath}</p>
+    <div className='movie-card'>
+      <h3>{title}</h3>
+      <img src={posterPath}/>
       <p>{averageRating}</p>
       <p>{releaseDate}</p>
-    </>
+    </div>
 )
 }
 
