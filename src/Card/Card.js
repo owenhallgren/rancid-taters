a334f764f06b989ee8665a66c3ffccef3898eca0
Max Bregman <bregmanmax91@gmail.com>
2021-03-25 15:33:41 -0600

    Make movie posters layout and add rounding function for ratings

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,14 @@
 import './CardStyles.css'
 
 const Card = ({ title, posterPath, averageRating, releaseDate }) => {
+
+  const roundedRating = Math.round(averageRating * 10)/10
+
 return (
     <div className='movie-card'>
       <h3>{title}</h3>
       <img src={posterPath}/>
-      <p>{averageRating}</p>
+      <p>{roundedRating}</p>
       <p>{releaseDate}</p>
     </div>
 )
