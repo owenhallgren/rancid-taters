ae4140548ab8b3adba8168c522df74f81cd350aa
Owen Hallgren <owenhallgren@gmail.com>
2021-03-26 18:51:51 -0600

    Started error catching

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -5,18 +5,18 @@ const MovieDetails = ({ title, posterPath, backDropPath,releaseDate, overview, a
     <div className='movie-details'>
     <button className='go-back-button'>Go Back</button>
       <img src={backDropPath} className='back-drop'/>
-      <section>
+      <section className='details-section'>
         <div>
           <img src={posterPath}/>
           <p>{averageRating} 🥔</p>
         </div>
-        <div>
+        <div className='details-section'>
           <h2>{title}</h2>
           <div className='genreDisplay'>{genres.map(genre => <p className='genre'>{genre} </p>)}</div>
           <p>{runtime} minutes</p>
           <p>{releaseDate}</p>
         </div>
-        <p className='overview'>{overview}</p>
+        <p className='overview details-section'>{overview}</p>
       </section>
     </div>
   )
